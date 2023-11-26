a374c56d40cc9fe9543245842366fb496381ca4e
Dawidooss <gok.daw@wp.pl>
2023-11-26 13:40:03 +0100

    hhee

--- src/client/Units/Pathfinding.ts
+++ src/client/Units/Pathfinding.ts
@@ -23,26 +23,10 @@ export default class Pathfinding {
 	private currentWaypointIndex = 0;
 	private pathId = "";
 
-	private visualisation: ActionCircle;
-	private visualisationPart: ActionCircle["Middle"];
-	private beamAttachment: Attachment;
-
 	constructor(unit: Unit) {
 		this.unit = unit;
 
 		this.path = PathfindingService.CreatePath(agentParams);
-
-		this.visualisation = ReplicatedFirst.FindFirstChild("NormalAction")!.Clone() as ActionCircle;
-		this.visualisationPart = this.visualisation.Middle;
-
-		this.visualisation.Name = "PathVisualisation";
-		this.visualisation.Parent = this.unit.model;
-		this.visualisation.Arrow.Destroy();
-		this.visualisationPart.Parent = undefined;
-
-		this.beamAttachment = new Instance("Attachment");
-		this.beamAttachment.Parent = this.unit.model.HumanoidRootPart;
-		this.beamAttachment.WorldCFrame = this.unit.model.GetPivot().mul(CFrame.Angles(0, math.pi, math.pi / 2));
 
 		// this.path.Blocked.Connect((blockedWaypointIndex) => {
 		// 	wait();
@@ -69,11 +53,6 @@ export default class Pathfinding {
 		this.MoveToCurrentWaypoint();
 	}
 	public Stop(success: boolean) {
-		// if (success) {
-		// const orientation = this.targetCFrame.ToOrientation();
-		// this.unit.alignOrientation.CFrame = CFrame.Angles(0, orientation[1], 0);
-		// }
-
 		this.active = false;
 		this.waypoints = [];
 		this.currentWaypointIndex = 0;
@@ -144,72 +123,6 @@ export default class Pathfinding {
 				this.unit.model.PivotTo(currentPosition);
 				this.Stop(false);
 			}
-		}
-	}
-
-	private ClearVisualisation() {
-		this.visualisation.Positions.ClearAllChildren();
-	}
-
-	private CreateVisualisation() {
-		print("create visualisation");
-		this.ClearVisualisation();
-		if (!this.active) return;
-
-		let previousVisualisationAtt = this.beamAttachment;
-
-		for (let waypointIndex = this.currentWaypointIndex; waypointIndex < this.waypoints.size(); waypointIndex++) {
-			const waypoint = this.waypoints[waypointIndex];
-			const toTargetCFrameDistance = previousVisualisationAtt.WorldPosition.sub(waypoint.Position).Magnitude;
-
-			const visualisationPart = this.visualisationPart.Clone();
-
-			const groundPositionResult = Utils.RaycastBottom(
-				waypoint.Position.add(new Vector3(0, 100, 0)),
-				[Workspace.TerrainParts],
-				Enum.RaycastFilterType.Include,
-			);
-			if (!groundPositionResult) continue;
-			const cframe = new CFrame(
-				groundPositionResult.Position,
-				groundPositionResult.Position.add(groundPositionResult.Normal),
-			).mul(CFrame.Angles(math.pi / 2, 0, 0));
-
-			visualisationPart.PivotTo(cframe);
-
-			visualisationPart.Beam.Attachment1 = previousVisualisationAtt;
-			visualisationPart.Beam.TextureLength = toTargetCFrameDistance;
-			visualisationPart.Name = `${this.pathId}#${waypointIndex}`;
-			visualisationPart.Transparency = waypointIndex === this.waypoints.size() - 1 ? 0 : 1;
-			visualisationPart.Parent = this.visualisation.Positions;
-
-			previousVisualisationAtt = visualisationPart.Attachment;
-		}
-	}
-
-	private UpdateVisualisation() {
-		for (let child of this.visualisation.Positions.GetChildren()) {
-			if (child.Name.split("#")[0] !== this.pathId) {
-				this.CreateVisualisation();
-				return;
-			}
-
-			const waypointIndex = tonumber(child.Name.split("#")[1] as string)!;
-			if (waypointIndex < this.currentWaypointIndex) {
-				child.Destroy();
-				continue;
-			}
-
-			const visualisationPart = child as ActionCircle["Middle"];
-			const toTargetCFrameDistance = visualisationPart.Beam.Attachment0!.WorldPosition.sub(
-				visualisationPart.Beam.Attachment1!.WorldPosition,
-			).Magnitude;
-
-			if (waypointIndex === this.currentWaypointIndex) {
-				visualisationPart.Beam.Attachment1 = this.beamAttachment;
-			}
-
-			visualisationPart.Beam.TextureLength = toTargetCFrameDistance;
 		}
 	}
 }
